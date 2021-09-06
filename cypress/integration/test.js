3f5cc5ea8562208e02dfe9a969d5db70ac6230a6
zoulei <1668218129@qq.com>
2021-09-06 15:06:55 +0800

    案列

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -2,45 +2,54 @@
 
 describe('My First Test', () => {
 
+  const username = 'admin'
+  const pwd = 'admin123'
+
   beforeEach(() => {
-    cy.visit('http://localhost/')
-    // cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__inner').type('admin');
-    // cy.get(':nth-child(3) > .el-form-item__content > .el-input > .el-input__inner').type('admin123');
-    // cy.get(':nth-child(4) > .el-form-item__content > .el-input > .el-input__inner').type('yzm');
-    // cy.get('.el-button').click();
-    // cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__inner')
-    // cy.get(':nth-child(5) > .el-submenu > .el-submenu__title').click();
-    // cy.get(':nth-child(5) > .el-submenu > .el-menu > :nth-child(1) > a > .el-menu-item').click();
+    // cy.visit(`${Cypress.config("baseUrl")}/login`)
+    // 调用 Custom Commands 的命令
+    cy.login('/login',username, pwd)
+
+
+    //更改视图窗口大小
+    // cy.viewport(1024,768)
+
+    //接口登录鉴权
+    // cy.loginRequest(username, pwd)
+    // //跳转首页
+    // cy.visit(`http://localhost/#/index`)
+
+    cy.wait(3000)
+    cy.url().should('include', '/index')
   })
 
-  it('大客户管理新增', () => {
-    cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__inner').type('admin').blur();
-    cy.get('.el-button').click();
+  it('大客户管理 -> 新增', () => {
+    cy.get(':nth-child(5) > .el-submenu > .el-submenu__title').click();
+    cy.get(':nth-child(5) > .el-submenu > .el-menu > :nth-child(1) > a > .el-menu-item').click()
+    cy.get('.el-button--primary.is-plain').click();
+
+    cy.get('.el-dialog__body > .el-form > :nth-child(1) > .el-form-item__content > .el-input > .el-input__inner')
+    .type('客户名称')
+    .should('have.value','客户名称');
+
+    cy.get('.el-dialog__body > .el-form > :nth-child(2) > .el-form-item__content > .el-input > .el-input__inner')
+    .type('税号')
+    .should('have.value','税号');
+
+    cy.get('.el-dialog__body > .el-form > :nth-child(3) > .el-form-item__content > .el-input > .el-input__inner')
+    .type('17716122141')
+    .should('have.value','17716122141');
+
+    cy.get('.dialog-footer > .el-button--primary').click();
+
   })
-  // it('大客户管理新增', () => {
-  //   cy.screenshot();
-    // cy.get('.el-button--primary.is-plain').click();
-    // cy.get('.el-dialog__body > .el-form > :nth-child(1) > .el-form-item__content > .el-input > .el-input__inner')
-    //     .type("大客户名称")
-    //     .should('have.value', '大客户名称');
-    // cy.get('.el-dialog__body > .el-form > :nth-child(2) > .el-form-item__content > .el-input > .el-input__inner')
-    //     .type("税号")
-    //     .should('have.value', '税号');
-    // cy.get('.el-dialog__body > .el-form > :nth-child(3) > .el-form-item__content > .el-input > .el-input__inner')
-    //     .type("17716122145")
-    //     .should('have.value', '17716122145');
-    // cy.get('.el-dialog__body > .el-form > :nth-child(4) > .el-form-item__content > .el-input > .el-input__inner')
-    //     .type("成都")
-    //     .should('have.value', '成都');
-    // cy.get('.el-dialog__body > .el-form > :nth-child(5) > .el-form-item__content > .el-input > .el-input__inner')
-    //     .type("对公账号")
-    //     .should('have.value', '对公账号');
-    // cy.get('.el-dialog__body > .el-form > :nth-child(6) > .el-form-item__content > .el-input > .el-input__inner')
-    //     .type("工商")
-    //     .should('have.value', '工商');
-    // cy.get('.dialog-footer > .el-button--primary').click({val:'xxx'});
-  // })
 
-
-
+  //应用程序抛出的异常，最终触发了 windows 全局错误处理器( global error handler ) 或者其 unhandledrejection handler，则 Cypress 同样会监测到此情况，并且以失败状态终止其测试。
+  //在 Cypress 里禁掉应用程序错误消息的完整代码：
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    console.log('Jerry errors!');
+    return false
+  })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,53 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//页面登录鉴权
+Cypress.Commands.add('login', (url, username, pwd) => {
+  // cy.visit(Cypress.env('baseUrl') + url);
+  // cy.visit(Cypress.env('baseUrl').baseUrl + url);
+  cy.visit(Cypress.config("baseUrl3") + url);
+    
+  cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__inner')
+  .type(username)
+  .should('have.value', username);
+
+  cy.get(':nth-child(3) > .el-form-item__content > .el-input > .el-input__inner')
+  .type(pwd)
+  .should('have.value',pwd);
+
+  cy.get(':nth-child(4) > .el-form-item__content > .el-input > .el-input__inner')
+  .type('默认验证码')
+  .should('have.value','默认验证码');
+  cy.get('.el-button').click();
+})
+
+
+//接口登录鉴权
+Cypress.Commands.add('loginRequest', (username, pwd) => {
+  // cy.visit(Cypress.config("baseApi3") + url);
+  Cypress.log({
+      name: 'login',
+      message: `${username} | ${pwd}`
+  })
+  //获取uuid
+  cy.request({
+    method: 'GET',
+    url: Cypress.config('baseApi') + '/code',
+  }).should((response) => {
+    //断言
+    expect(response.status).to.eq(200)
+    //登录
+    cy.request('POST', Cypress.config('baseApi') + '/auth/login',{
+      username: username,
+      password: pwd, 
+      code: 'xxx',
+      uuid: response.body.uuid
+    }).should((response) => {
+      //断言
+      expect(response.status).to.eq(200)
+      //设置Cookie
+      cy.setCookie('Admin-Token',response.body.data.access_token)
+    })
+  })
+})
